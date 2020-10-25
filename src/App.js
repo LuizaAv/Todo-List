638b10d336e209a1014cc2998107350cd9f66524
LuizaAv <l.avetisyan7777@gmail.com>
2020-10-25 21:09:49 +0400

    todos adding functionality

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [inputText, setInputText]  = useState("")
+  const [inputText, setInputText]  = useState("");
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <header>
       <h1>Luiza's Todo list</h1>
       </header>
-      <Form/>
+      <Form  
+      inputText = {inputText}
+      todos = {todos} 
+      setTodos = {setTodos} 
+      setInputText = {setInputText}/>
       <TodoList/>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,22 @@
 import React from "react"
 
-const Form = () => {
+const Form = ({inputText, setInputText, todos, setTodos}) => {
     const  inputTextHandler = (e) => {
-        console.log(e.target.value)
+        setInputText(e.target.value)
     }
+
+    const clickHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos, {text: inputText, completed: false, id: Math.random() * 1000}
+        ])
+        //console.log(todos[0])
+    }
+
     return(
         <form>
             <input type="text" className="todo-input"  onChange={inputTextHandler}/>
-            <button className="todo-button"  type = "submit">
+            <button className="todo-button"  type = "submit"  onClick= {clickHandler}>
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
